7e9f98f432a0b5865451824ff377d4711ad0630a
Haukur Kristinsson <haukur@hauxi.is>
2014-07-21 11:55:36 +0000

    Installation script typo Vol 2.

--- install.sh
+++ install.sh
@@ -11,9 +11,9 @@ echo "done"
 echo -n "Changing to the $dotfiles_dir directory ..."
 cd $dotfiles_dir
 echo "done"
-for file in $files; do
+for file in $dotfiles; do
     echo "Moving any existing dotfiles from ~ to $bakdir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file ~/dotfiles_bak/                                                                                                         
     echo "Creating symlink to $file in home directory."
     ln -s $dotfiles_dir/$file ~/.$file
 done
